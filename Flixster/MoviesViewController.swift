e081172b4f3a6f5207c878a826a7619dbfa974b8
Ajuogaaz <brianlinus1753@gmail.com>
2020-10-04 16:46:45 -0500

    Added new screen and configured click to that screen

--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -72,16 +72,31 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
-    
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("loading details")
+        
+        //Find the selected Movie
+        let cell  = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie details to the view controller
+        var detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at : indexPath, animated : true)
+        
+        
     }
-    */
+    
 
 }
